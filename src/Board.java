e654ce0bf1b17a1b9332444f86f5c981e550b450
Huskydog9988 <39809509+Huskydog9988@users.noreply.github.com>
2021-05-11 09:28:46 -0400

    Update Board.java

--- src/Board.java
+++ src/Board.java
@@ -4,18 +4,37 @@ public class Board {
      * A "true" means a cell is alive, a "false" means its dead
      */
     private boolean[][] board;
+    /**
+     * Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+     */
     private int underpopulation = 2;
+    /**
+     * Any live cell with two or three live neighbours lives on to the next generation.
+     */
     private int[] livesOn = {2, 3};
+    /**
+     * Any live cell with more than three live neighbours dies, as if by overpopulation.
+     */
+    private int overpopulation = 4;
+    /**
+     * Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+     */
+    private int reproduction = 3;
 
+    /**
+     * create a new board for the game
+     * @param rows how many rows
+     * @param cols how many columns
+     */
     Board(int rows, int cols) {
         board = new boolean[rows][cols];
     }
 
     /**
      * Gets a cell at the specified position
-     * @param row
-     * @param col
-     * @return
+     * @param row the row
+     * @param col the column
+     * @return value at specified position
      */
     public boolean getCellAt(int row, int col) {
         return board[row][col];
@@ -24,8 +43,8 @@ public class Board {
     /**
      * Sets a cell at the specified position to it's opposite value
      * A "false" become a "true" and vice versa
-     * @param row
-     * @param col
+     * @param row the row
+     * @param col the column
      */
     public void setCellAt(int row, int col) {
         board[row][col] = !board[row][col];
@@ -38,7 +57,7 @@ public class Board {
 
     /**
      * get underpopulation value
-     * @return
+     * @return underpopulation
      */
     public int getUnderpopulation() {
         return underpopulation;
@@ -46,7 +65,7 @@ public class Board {
 
     /**
      * set underpopulation value
-     * @param value
+     * @param value new underpopulation
      */
     public void setUnderpopulation(int value) {
         underpopulation = value;
@@ -54,7 +73,7 @@ public class Board {
 
     /**
      * get livesOn value
-     * @return
+     * @return livesOn
      */
     public int[] getLivesOn() {
         return livesOn;
@@ -62,11 +81,43 @@ public class Board {
 
     /**
      * sets livesOn value
-     * @param lower
-     * @param upper
+     * @param lower the lower bounds to live on
+     * @param upper the upper (inclusive) bound to live on
      */
     public void setLivesOn(int lower, int upper) {
         livesOn[0] = lower;
         livesOn[1] = upper;
+    }
+
+    /**
+     * gets overpopulation value
+     * @return overpopulation
+     */
+    public int getOverpopulation() {
+        return overpopulation;
+    }
+
+    /**
+     * sets overpopulation value
+     * @param value new overpopulation
+     */
+    public void setOverpopulation(int value) {
+        overpopulation = value;
+    }
+
+    /**
+     * gets reproduction value
+     * @return reproduction
+     */
+    public int getReproduction() {
+        return reproduction;
+    }
+
+    /**
+     * sets reproduction value
+     * @param value new reproduction
+     */
+    public void setReproduction(int value) {
+        reproduction = value;
     }
 }
